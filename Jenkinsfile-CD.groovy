de82fa68000664a1c49ae9603f3b3b404bfcc13d
SalomonEslaitEndava <salomon-antonio.eslait@endava.com>
2022-04-21 18:15:32 -0500

    CD test

--- Jenkinsfile-CD.groovy
+++ Jenkinsfile-CD.groovy
@@ -1,3 +1,4 @@
+def miVariable = Jenkins.instance.getItem('rampup-CI').lastSuccessfulBuild.number
 pipeline {
     environment {
         registry = "salomoneslaitendava/devopsrampup-front"
@@ -13,6 +14,7 @@ pipeline {
                     ssh -o StrictHostKeyChecking=no -l salomon_antonio_eslait 10.0.1.30 'touch sobeloenlaotramaquina.txt'
                     
                     '''
+                    sh "echo mi variable es ${miVariable}"
                     // sh 'ssh salomon_antonio_eslait@10.0.1.26 git clone https://github.com/SalomonEslaitEndava/kubernetes.git'
                     // sh 'ssh salomon_antonio_eslait@10.0.1.26 pwd'
                 }
