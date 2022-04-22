e5a25be89111525ff0b41d562d24ff77634490e7
SalomonEslaitEndava <salomon-antonio.eslait@endava.com>
2022-04-21 20:10:06 -0500

    CD pipeline full test

--- Jenkinsfile-CD.groovy
+++ Jenkinsfile-CD.groovy
@@ -13,9 +13,6 @@ pipeline {
                     sh """
                     ssh -o StrictHostKeyChecking=no -l salomon_antonio_eslait 10.0.1.30 'rm -r kubernetes'
                     ssh -o StrictHostKeyChecking=no -l salomon_antonio_eslait 10.0.1.30 'git clone https://github.com/SalomonEslaitEndava/kubernetes.git'
-                    ssh -o StrictHostKeyChecking=no -l salomon_antonio_eslait 10.0.1.30 'pwd'
-                    ssh -o StrictHostKeyChecking=no -l salomon_antonio_eslait 10.0.1.30 'cd kubernetes'
-                    ssh -o StrictHostKeyChecking=no -l salomon_antonio_eslait 10.0.1.30 'ls'
                     ssh -o StrictHostKeyChecking=no -l salomon_antonio_eslait 10.0.1.30 'sed -i 's/devopsrampup-front:latest/devopsrampup-front:${ciLastBuild}/' ./kubernetes/deploy-frontend.yaml'
                     ssh -o StrictHostKeyChecking=no -l salomon_antonio_eslait 10.0.1.30 'kubectlapply -f deploy-frontend.yaml'
                     ssh -o StrictHostKeyChecking=no -l salomon_antonio_eslait 10.0.1.30 'rm deploy-frontend.yaml'
